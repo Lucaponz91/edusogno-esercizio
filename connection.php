fe9d9de52f82e5f7ca2bceee8df705f5a586d291
Lucaponz91 <lucapo1991@msn.com>
2022-12-19 16:18:10 +0100

    changed pdo into mysqli, add index

--- connection.php
+++ connection.php
@@ -1,23 +1,15 @@
 <?php
-// connessione al DB
-$config = [
-    'db_engine' => 'mysql',
-    'db_host' => '127.0.0.1',
-    'db_name' => 'edusogno',
-    'db_user' => 'root',
-    'db_password' => 'root',
-];
 
-$db_config = $config['db_engine'] . ":host=".$config['db_host'] . ";dbname=" . $config['db_name'];
+$host =	"127.0.0.1";
+$user = "root";
+$pwd =	"root";
+$db = "edusogno";
 
-// "Proviamo" la connessione al DB. Se non va a buon fine lanciamo la PDOException
-try {
-    $pdo = new PDO($db_config, $config['db_user'], $config['db_password'], [
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-    ]);
-        
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-} catch (PDOException $e) {
-    exit("Impossibile connettersi al database: " . $e->getMessage());
+$connessione = new mysqli ($host,$user,$pwd,$db);
+
+if($connessione === false){
+    die("Tentativo di connessione fallito. Errore: " . $connessione->connect_error);
+
 }
+
+?>
--- index.php
+++ index.php
@@ -1,15 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Edusogno</title>
+        <link rel="stylesheet" href="assets/styles/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">     
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+        <script src="assets/js/script.js" defer></script> 
+    </head>
+    <body>
+        
+        <h1>Hai già un account</h1>
+        <div class="register">
+            
+        <form class="account" action="login.php" method="post" id="login">
+            <label for="email">Inserisci l'email</label>
+            <input type="email" id="email" placeholder="name@example.com" name="email" required/>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edusogno</title>
-</head>
+            <label for="password">Inserisci la password</label>
+            <div class="togglePwd">
+                <input type="password" id="password" placeholder="Scrivila qui" name="password" required/>
+                <i class="far fa-eye" id="togglePassword" style="margin-left: -25px; cursor: pointer;"></i>
+            </div>
+            <input type="submit" value="ACCEDI" />
+            <a class="log-register" href="registration.php">Non hai ancora un profilo? <strong>Registrati</strong></a>
+        </form>
 
-<body>
-
-</body>
-
+        </div>
+        
+    </body>
 </html>
